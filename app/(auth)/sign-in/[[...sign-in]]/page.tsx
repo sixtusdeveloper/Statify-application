82cb79639c10bd12b13a7c43b54fd7668582b400
sixtusdeveloper <sixtusushrey@gmail.com>
2024-07-18 20:59:25 +0100

    commit on few files

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs';// Assuming the correct file name is 'SignUp' with a capital 'S'
+
+const SignInPage = () => {
+  return <SignIn />
+}
+
+export default SignInPage
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs';// Assuming the correct file name is 'SignUp' with a capital 'S'
+
+const SignUpPage = () => {
+  return <SignUp />
+}
+
+export default SignUpPage
